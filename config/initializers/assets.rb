132543aaeb94749a28d5dd2534f857087a8e4131
leahjs <leahj.suter@gmail.com>
2015-06-14 12:46:30 -0400

    added custom fonts but not working

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .otf)
+
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
