34aacc9d764409d1b30c7aa0f8383432c1993951
leahjs <leahj.suter@gmail.com>
2015-06-15 15:18:26 -0400

    attempt to add a different font to each date

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,14 @@
 class Day < ActiveRecord::Base
+
+  def self.load_fonts
+    font_of_day = []
+    File.open("app/assets/stylesheets/fonts365.txt", "r") do |f|
+      f.each_line do |font_name|
+        font_of_day << font_name
+    end
+    font_of_day
+  end
 end
+
+end
+
