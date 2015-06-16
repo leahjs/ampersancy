ed93be5b565dd89960924f72e07bae96681e6a16
leahjs <leahj.suter@gmail.com>
2015-06-16 12:10:31 -0400

    add link to div

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,14 +1,16 @@
 class Day < ActiveRecord::Base
 
+
+# add fonts and ampersands to database when I find all 365 fonts
   def self.load_fonts
     font_of_day = []
     File.open("app/assets/stylesheets/fonts365.txt", "r") do |f|
       f.each_line do |font_name|
         font_of_day << font_name
-    end
+      end
     font_of_day
+    end
   end
-end
 
 end
 
